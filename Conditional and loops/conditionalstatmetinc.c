5b8438e92d2c547188bd832e1ea2054c2276833c
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-02-02 18:04:09 +0545

    Update conditionalstatmetinc.c

--- Conditional and loops/conditionalstatmetinc.c
+++ Conditional and loops/conditionalstatmetinc.c
@@ -19,35 +19,9 @@ int main()
     int n = strtol(n_str, &n_endptr, 10);
 
     if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
-
-    if(n >= 1 && n<=9){
-        if(n==1){
-            printf("one");
-        }
-        else if(n==2){
-            printf("two");
-        }
-        else if(n==3){
-            printf("three");
-        }
-        else if(n==4){
-            printf("four");
-        }
-        else if(n==5){
-            printf("five");
-        }
-        else if(n==6){
-            printf("six");
-        }
-        else if(n==7){
-            printf("severn");
-        }
-        else if(n==8){
-            printf("eight");
-        }
-        else if(n==9){
-            printf("nine");
-        }
+    char nums[][9]={"","one","two","three","four","five","six","seven","eight","nine"};
+    if(n>0 && n<10){
+        printf("%s",nums[n]);
     }
     else{
         printf("Greater than 9");
