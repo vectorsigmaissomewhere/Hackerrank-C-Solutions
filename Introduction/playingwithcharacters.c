39cb73b5ea6a95007bf89caec94f2c25d1bb4a04
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-02-02 12:53:32 +0545

    Create playingwithcharacters.c

---
+++ Introduction/playingwithcharacters.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char ch[100];
+    char s[100];
+    char sen[100];
+    scanf("%c",&ch);
+    scanf("\n");
+    scanf("%[^\n]%*c", &s);
+    scanf("\n");
+    scanf("%[^\n]%*c", &sen);
+    printf("%s\n",ch);
+    printf("%s\n",s);
+    printf("%s",sen);   
+    return 0;
+}
+/*
+    INPUT
+C
+Language
+Welcome To C!!
+
+  OUTPUT
+C
+Language
+Welcome To C!!
+*/
