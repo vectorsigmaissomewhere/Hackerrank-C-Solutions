66ac982c4815efee68ffb0c73c5c8ba0c1917449
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-02-02 15:02:08 +0545

    Create pointersinc.c

---
+++ Introduction/pointersinc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    *a = *a + *b;
+    *b = abs(*a - (2**b));           
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+
+
+/*
+  INPUT
+4
+5
+  OUTPUT
+9
+1
+*/
